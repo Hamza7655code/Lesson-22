128101681cff82da1bf37446cbb244396895c368
Hamza7655code <mughis.rehman@yahoo.com>
2025-07-03 18:06:20 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,14 @@
+'''Write a program to perform the following operations: 1. Create a tuple with different datatypes 2. Create another tuple of integers 3. Create a new tuple by adding 9 to the previous tuple 4. Count the occurrences of an element in the tuple 5. Perform slicing on the tuple'''
+t1 = (1, 'a' 'g', 99, 'hello')
+print(t1)
+t2 = (9,8,7,6,5,4,3,2,1)
+print(t2)
+t3 = (9,)
+t4 = t2 + t3
+print(t4)
+t5 = ('g', 5, 9,'g','g',4)
+print (t5.count('g'))
+t6 = (1,2,3,'h','i','j')
+print(t6[1:5])
+
+
